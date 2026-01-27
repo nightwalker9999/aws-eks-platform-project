1842abf30adb849b2d2937840de2e0947942d982
root <sharmaarjun99998888@gmail.com>
2026-01-27 08:04:59 +0000

    Add subnets, IGW, and routing to VPC module

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -12,3 +12,40 @@ resource "aws_vpc" "this" {
   }
 }
 
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-public-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+
